5f7890af6e0cf91d77cff11b13eca98738091d65
Ping Zou <ping.zou@nagra.com>
2016-09-23 11:27:50 +0100

    refactor to support full URI in the master and stream playlist

--- download_hls.py
+++ download_hls.py
@@ -26,77 +26,95 @@ def download(uri, filename):
         f.write(resp.content)
 
 
-def download_uri(host_root, subpath, clear_local=False):
+def download_uri(uri, local, clear_local=False):
     '''
-    Download resource from host_root+subpath
-    save to subpath, i.e. path/to/target.file (relative to CWD)
+    Download resource from uri, save to local, i.e. path/to/target.file (relative to CWD)
 
-    @param subpath path/to/target.file
+    @param uri Full URI of the target
+    @param local path/to/target.file
     '''
-    print('downloading {uri} to {local}'.format(uri=host_root+subpath, local=subpath))
-    if os.path.exists(subpath):
+    print('downloading {uri} to {local}'.format(uri=uri, local=local))
+    if os.path.exists(local):
         if clear_local:
-            os.remove(subpath)
+            os.remove(local)
         else:
-            print('{subpath} exists'.format(subpath=subpath))
+            print('{local} exists'.format(local=local))
             return
 
-    target_name = os.path.basename(subpath)
-    subfolder = os.path.dirname(subpath)
+    target_name = os.path.basename(local)
+    subfolder = os.path.dirname(local)
     if subfolder and not os.path.exists(subfolder):
         os.makedirs(subfolder)
 
-    download(host_root+subpath, target_name)
-    os.rename(target_name, subpath)
+    download(uri, target_name)
+    os.rename(target_name, local)
 
 
-def download_master_playlist(host_root, master_playlist_file):
+def download_master_playlist(master_playlist_uri):
     '''
-    @param host_root
-    @param master_playlist+file
+    @param master_playlist_uri Full URI of master playlist
     '''
-    master_playlist_uri = host_root + master_playlist_file
-    download_uri(host_root, master_playlist_file)
+    host, subpath, filename = parse_uri(master_playlist_uri)
+    download_uri(master_playlist_uri, filename, clear_local=True)  # always refresh playlist - so the script can be used for parsing live streams
     print('loading {master_playlist_uri}'.format(master_playlist_uri=master_playlist_uri))
-    with open(master_playlist_file, 'r') as f:
+    with open(filename, 'r') as f:
         content = f.read()
     master_playlist = m3u8.loads(content)
     return master_playlist
 
 
-def download_stream(host_root, stream_playlist_file):
+def is_full_uri(uri):
+    return uri.startswith('http://') or uri.startswith('https://')
+
+
+def download_stream(uri, subpath=None):
     '''
-    @param host_root
-    @param stream_playlist_file
+    @param uri
+    @param subpath Relative subpath, if None, use uri's subpath
     '''
-    download_uri(host_root, stream_playlist_file)
-    stream_playlist_uri = host_root + stream_playlist_file
-    print('loading {stream_playlist_uri}'.format(stream_playlist_uri=stream_playlist_uri))
-    with open(stream_playlist_file, 'r') as f:
+    stream_playlist_host, stream_playlist_subpath, stream_playlist_filename = parse_uri(uri)
+    local_stream_playlist_filename = os.path.join(stream_playlist_subpath, stream_playlist_filename)
+    if subpath:
+        local_stream_playlist_filename = os.path.join(subpath, stream_playlist_filename)
+    download_uri(uri, local_stream_playlist_filename, clear_local=True)   # always refresh playlist - so the script can be used for parsing live streams
+
+    print('loading {local}'.format(local=local_stream_playlist_filename))
+    with open(local_stream_playlist_filename, 'r') as f:
         content = f.read()
     stream_playlist = m3u8.loads(content)
 
-    subpath = os.path.dirname(stream_playlist_file)
-    print('subpath:', subpath)
     for segment in stream_playlist.segments:
-        download_uri(host_root, '/'.join([subpath, segment.uri]))
+        if segment.uri.startswith('#'):
+            continue
+        if is_full_uri(segment.uri):
+            segment_host, segment_subpath, segment_filename = parse_uri(segment.uri)
+            download_uri(segment.uri, os.path.join(segment_subpath, segment_filename))
+        else:
+            segment_uri = stream_playlist_host + '/' + stream_playlist_subpath + '/' + segment.uri
+            download_uri(segment_uri, os.path.join(subpath, segment.uri))
         if segment.byterange:
             break
 
 
 def parse_uri(uri):
     '''
-    parse URI into host_root, filename
+    parse URI into host_root, subpath, filename
+
     e.g. http://example.com/path/to/stream_id/index.m3u8
-    host_root = http://example.com/path/to/stream_id/
-    filename = index.m3u8
+        host_root = http://example.com
+        subpath = path/to/stream_id
+        filename = index.m3u8
+
     @param uri
-    @return host_root, filename
+    @return host_root, subpath, filename
     '''
+    print('parsing {uri}'.format(uri=uri))
     url = urlparse(uri)
-    host_root = url.scheme + '://' + url.netloc + os.path.dirname(url.path) + '/'
+    host_root = url.scheme + '://' + url.netloc
+    subpath = os.path.dirname(url.path)[1:]
     filename = os.path.basename(url.path)
-    return host_root, filename
+
+    return host_root, subpath, filename
 
 
 def download_hls_stream(master_playlist_uri, id='.'):
@@ -106,17 +124,21 @@ def download_hls_stream(master_playlist_uri, id='.'):
     @param id Defaut CWD
     '''
     print('master_playlist_uri: {master_playlist_uri}'.format(master_playlist_uri=master_playlist_uri))
-    host_root, master_playlist_file = parse_uri(master_playlist_uri)
     local_root = id
 
-    print('downloading {uri} to {local}'.format(uri=host_root+master_playlist_file, local=local_root))
+    print('downloading {uri} to {local}'.format(uri=master_playlist_uri, local=local_root))
     old_cwd = chdir(local_root)
 
-    master_playlist = download_master_playlist(host_root, master_playlist_file)
+    master_playlist = download_master_playlist(master_playlist_uri)
 
+    host_root, subpath, master_playlist_file = parse_uri(master_playlist_uri)
     # download resource from stream playlist
     for playlist in master_playlist.playlists:
-        download_stream(host_root, playlist.uri)
+        if is_full_uri(playlist.uri):
+            download_stream(playlist.uri)
+        else:
+            playlist_uri = host_root + '/' + subpath + '/' + playlist.uri
+            download_stream(playlist_uri, os.path.dirname(playlist.uri))
 
     # download resource from URI
     pattern = 'URI="(.*)"'
@@ -124,12 +146,20 @@ def download_hls_stream(master_playlist_uri, id='.'):
         m = re.search(pattern, l)
         if m:
             uri = m.group(1)
-            download_stream(host_root, uri)
+            if is_full_uri(uri):
+                download_stream(uri)
+            else:
+                playlist_uri = host_root + '/' + subpath + '/' + uri
+                download_stream(playlist_uri, os.path.dirname(uri))
 
     os.chdir(old_cwd)
 
 
 def main():
+
+    # root, subpath, filename = parse_uri('http://devimages.apple.com.edgekey.net/bipbop_4x3_variant.m3u8')
+    # print(root, subpath, filename)
+
     parser = argparse.ArgumentParser(description='Download HLS streams -- default to download Apple reference HLS streams')
     parser.add_argument('--stream', help='master playlist URI, m3u8 resource')
     parser.add_argument('--id', help='stream id. Used a subfolder name for the downloads')
